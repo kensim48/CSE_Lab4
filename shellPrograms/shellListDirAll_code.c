8b5a4cfcde88a2444066225384fcba2340ea4066
Kenneth Sim <kensim28@hotmail.com>
2019-03-07 22:04:54 +0800

    feature: listalldir

--- shellPrograms/shellListDirAll_code.c
+++ shellPrograms/shellListDirAll_code.c
@@ -6,15 +6,43 @@
 //  Copyright © 2019 Natalie Agus. All rights reserved.
 //
 
-
 #include "shellPrograms.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-int shellListDirAll_code(char** args)
+void recursion(DIR *d)
 {
-	printf("Hello from shellListDirAll_code. Unfortunately, 'listdirall' hasn't been implemented yet.");
+    struct dirent *dir;
+
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            printf("%s\n", dir->d_name);
+            struct stat path_stat;
+            char c = dir->d_name[0];
+            stat(dir->d_name, &path_stat);
+            if (S_ISREG(path_stat.st_mode) == 0 && c!='.')
+            {
+                DIR *e;
+                e = opendir(dir->d_name);
+                recursion(e);
+            }
+        }
+        closedir(d);
+    }
+}
+
+int shellListDirAll_code(char **args)
+{
+    DIR *d;
+    d = opendir(".");
+    recursion(d);
     return 1;
 }
 
-int main(int argc, char** args){
+int main(int argc, char **args)
+{
     return shellListDirAll_code(args);
 }
